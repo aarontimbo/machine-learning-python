f11528cc882304fd9c65e8e74a23fd26a70c0d21
Aaron Timbo <aaron.timbo@gmail.com>
2018-03-03 07:34:02 -0600

    Convolutional Neuro Nets Resnets with Keras

---
+++ convolutional_nn_resnets_with_keras.py
@@ -0,0 +1,227 @@
+# Convolutional Neural Network
+# coursera notebook - Week 2
+# Residual Networks (ResNets)
+# for building deep NN's with Keras
+
+import numpy as np
+from keras import layers
+from keras.layers import Input, Add, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D, AveragePooling2D, MaxPooling2D, GlobalMaxPooling2D
+from keras.models import Model, load_model
+from keras.preprocessing import image
+from keras.utils import layer_utils
+from keras.utils.data_utils import get_file
+from keras.applications.imagenet_utils import preprocess_input
+import pydot
+from IPython.display import SVG
+from keras.utils.vis_utils import model_to_dot
+from keras.utils import plot_model
+from resnets_utils import *
+from keras.initializers import glorot_uniform
+import scipy.misc
+from matplotlib.pyplot import imshow
+%matplotlib inline
+
+import keras.backend as K
+K.set_image_data_format('channels_last')
+K.set_learning_phase(1)
+
+# GRADED FUNCTION: identity_block
+
+def identity_block(X, f, filters, stage, block):
+    """
+    Implementation of the identity block as defined in Figure 3
+    
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
+    
+    Returns:
+    X -- output of the identity block, tensor of shape (n_H, n_W, n_C)
+    """
+    
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    # Retrieve Filters
+    F1, F2, F3 = filters
+    
+    # Save the input value. You'll need this later to add back to the main path. 
+    X_shortcut = X
+    
+    # First component of main path
+    X = Conv2D(filters = F1, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+    
+    ### START CODE HERE ###
+    
+    # Second component of main path (≈3 lines)
+    X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    # Third component of main path (≈2 lines)
+    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+    
+    ### END CODE HERE ###
+    
+    return X
+
+tf.reset_default_graph()
+
+with tf.Session() as test:
+    np.random.seed(1)
+    A_prev = tf.placeholder("float", [3, 4, 4, 6])
+    X = np.random.randn(3, 4, 4, 6)
+    A = identity_block(A_prev, f = 2, filters = [2, 4, 6], stage = 1, block = 'a')
+    test.run(tf.global_variables_initializer())
+    out = test.run([A], feed_dict={A_prev: X, K.learning_phase(): 0})
+    print("out = " + str(out[0][1][1][0]))
+
+"""
+out = [ 0.94822985  0.          1.16101444  2.747859    0.          1.36677003]
+"""
+
+# GRADED FUNCTION: convolutional_block
+
+def convolutional_block(X, f, filters, stage, block, s = 2):
+    """
+    Implementation of the convolutional block as defined in Figure 4
+    
+    Arguments:
+    X -- input tensor of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    f -- integer, specifying the shape of the middle CONV's window for the main path
+    filters -- python list of integers, defining the number of filters in the CONV layers of the main path
+    stage -- integer, used to name the layers, depending on their position in the network
+    block -- string/character, used to name the layers, depending on their position in the network
+    s -- Integer, specifying the stride to be used
+    
+    Returns:
+    X -- output of the convolutional block, tensor of shape (n_H, n_W, n_C)
+    """
+    
+    # defining name basis
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+    
+    # Retrieve Filters
+    F1, F2, F3 = filters
+    
+    # Save the input value
+    X_shortcut = X
+
+
+    ##### MAIN PATH #####
+    # First component of main path 
+    X = Conv2D(F1, (1, 1), strides = (s,s), name = conv_name_base + '2a', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2a')(X)
+    X = Activation('relu')(X)
+    
+    ### START CODE HERE ###
+
+    # Second component of main path (≈3 lines)
+    X = Conv2D(filters = F2, kernel_size = (f, f), strides = (1,1), padding = 'same', name = conv_name_base + '2b', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2b')(X)
+    X = Activation('relu')(X)
+
+    # Third component of main path (≈2 lines)
+    X = Conv2D(filters = F3, kernel_size = (1, 1), strides = (1,1), padding = 'valid', name = conv_name_base + '2c', kernel_initializer = glorot_uniform(seed=0))(X)
+    X = BatchNormalization(axis = 3, name = bn_name_base + '2c')(X)
+
+    ##### SHORTCUT PATH #### (≈2 lines)
+    X_shortcut = Conv2D(filters = F3, kernel_size = (1, 1), strides = (s,s), padding = 'valid', name = conv_name_base + '1', kernel_initializer = glorot_uniform(seed=0))(X_shortcut)
+    X_shortcut = BatchNormalization(axis = 3, name = bn_name_base + '1')(X_shortcut)
+
+    # Final step: Add shortcut value to main path, and pass it through a RELU activation (≈2 lines)
+    X = Add()([X, X_shortcut])
+    X = Activation('relu')(X)
+    
+    ### END CODE HERE ###
+    
+    return X
+
+tf.reset_default_graph()
+
+with tf.Session() as test:
+    np.random.seed(1)
+    A_prev = tf.placeholder("float", [3, 4, 4, 6])
+    X = np.random.randn(3, 4, 4, 6)
+    A = convolutional_block(A_prev, f = 2, filters = [2, 4, 6], stage = 1, block = 'a')
+    test.run(tf.global_variables_initializer())
+    out = test.run([A], feed_dict={A_prev: X, K.learning_phase(): 0})
+    print("out = " + str(out[0][1][1][0]))
+
+"""
+out = [ 0.09018463  1.23489773  0.46822017  0.0367176   0.          0.65516603]
+"""
+
+X_train_orig, Y_train_orig, X_test_orig, Y_test_orig, classes = load_dataset()
+
+# Normalize image vectors
+X_train = X_train_orig/255.
+X_test = X_test_orig/255.
+
+# Convert training and test labels to one hot matrices
+Y_train = convert_to_one_hot(Y_train_orig, 6).T
+Y_test = convert_to_one_hot(Y_test_orig, 6).T
+
+print ("number of training examples = " + str(X_train.shape[0]))
+print ("number of test examples = " + str(X_test.shape[0]))
+print ("X_train shape: " + str(X_train.shape))
+print ("Y_train shape: " + str(Y_train.shape))
+print ("X_test shape: " + str(X_test.shape))
+print ("Y_test shape: " + str(Y_test.shape))
+
+"""
+number of training examples = 1080
+number of test examples = 120
+X_train shape: (1080, 64, 64, 3)
+Y_train shape: (1080, 6)
+X_test shape: (120, 64, 64, 3)
+Y_test shape: (120, 6)
+"""
+
+model.fit(X_train, Y_train, epochs = 2, batch_size = 32)
+
+"""
+Epoch 1/2
+1080/1080 [==============================] - 576s - loss: 2.8333 - acc: 0.2630    
+Epoch 2/2
+1080/1080 [==============================] - 394s - loss: 2.6654 - acc: 0.3454  
+"""
+
+preds = model.evaluate(X_test, Y_test)
+print ("Loss = " + str(preds[0]))
+print ("Test Accuracy = " + str(preds[1]))
+
+"""
+120/120 [==============================] - 14s    
+Loss = 2.77988853455
+Test Accuracy = 0.166666666667
+"""
+
+
+"""
+What you should remember:
+
+* Very deep "plain" networks don't work in practice because they are hard to train due to vanishing gradients.
+* The skip-connections help to address the Vanishing Gradient problem. They also make it easy for a ResNet block to learn an identity function.
+* There are two main type of blocks: The identity block and the convolutional block.
+* Very deep Residual Networks are built by stacking these blocks together.
+
+References
+This notebook presents the ResNet algorithm due to He et al. (2015). The implementation here also took significant inspiration and follows the structure given in the github repository of Francois Chollet:
+
+Kaiming He, Xiangyu Zhang, Shaoqing Ren, Jian Sun - Deep Residual Learning for Image Recognition (2015)
+Francois Chollet's github repository: https://github.com/fchollet/deep-learning-models/blob/master/resnet50.py
+"""
+
